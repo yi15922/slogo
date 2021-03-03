7d124ff1fcacdd0ffbde7b309972ba1492cf9d16
Kenneth Moore III <km460@duke.edu>
2021-03-03 00:28:48 -0500

    upgraded turtle API

--- doc/plan/api/Turtle.java
+++ doc/plan/api/Turtle.java
@@ -1,8 +1,29 @@
 package slogo;
 
+/**
+ * The Turtle Interface is an API that provides publically accessable methods.
+ */
 public Interface Turtle{
-    public int move();
-    public int turn();
-    public int togglePen();
-    public int toggleVisibility();
-    }
+
+  /**
+  * The move method changes the turtles position by x in the x direction and y in y direction.
+   * It throws an excpetion if the move puts the turle out of bounds
+   * It returns how much it has moved.
+  */
+  public int move(int x, int y) throws Exception;
+
+  /**
+  * The turn method turns the turtle a degrees amount.  It returns the amount moved.
+  */
+  public int turn(int degrees);
+
+  /**
+  * The method toggles the value of the pen to be either up or down.
+  */
+  public void togglePen();
+
+  /**
+  * The method toggles the booleann visible to be either visible or not.  
+  */
+  public void toggleVisibility();
+}
