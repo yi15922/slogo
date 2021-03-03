417f48a5cf024dff7ee7b4c52e906caa786062f0
Liam Idrovo <lai3@duke.edu>
2021-03-03 03:06:19 -0500

    Added wireframe and wrote iew interface

---
+++ src/slogo/Forward.java
@@ -0,0 +1,25 @@
+package slogo;
+
+import java.util.List;
+
+public class Forward implements Command{
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public void setParameters(List<String> parameters) {
+
+    }
+
+    @Override
+    public String getCommandName() {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfParameters() {
+        return 0;
+    }
+}
