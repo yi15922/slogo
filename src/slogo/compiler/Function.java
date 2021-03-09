76ca1082bd133802f92a1a7fb1c37996f82f0d91
Patrick Liu <pyl8@duke.edu>
2021-03-08 22:49:53 -0500

    Refactored Function into a class

--- src/slogo/compiler/Function.java
+++ src/slogo/compiler/Function.java
@@ -1,8 +1,10 @@
 package slogo.compiler;
 
 import java.util.Collection;
+import slogo.SLogoException;
 import slogo.compiler.SLogoRunnable;
 import slogo.compiler.Token;
+import slogo.compiler.command.Command;
 
 /**
  * A {@code Function} is a {@code Token} than extends {@code SLogoRunnable} and
@@ -13,7 +15,36 @@ import slogo.compiler.Token;
  *
  * @author Yi Chen
  */
-public interface Function extends SLogoRunnable, slogo.WorkspaceEntry {
+public class Function extends Token implements SLogoRunnable {
+  protected Collection<Token> tokenList;
+  protected Collection<Command> commandList;
+
+  /**
+   * All Tokens must be initialized with a name, which is almost always the contents of the String
+   * that is being converted into a Token. For Function objects, this name will act as the
+   * name of the function.
+   *
+   * @param name - the specified name of the Token
+   */
+  public Function(String name) {
+    super(name);
+  }
+
+  /**
+   * Compiler is responsible for ensuring that the first {@code Token} given to the {@code Function}
+   * is a {@code Command} since SLogo syntax dictates that all user interactions are in the form of commands
+   * @param initCommand
+   * @param parameterTokens
+   * @throws SLogoException
+   */
+  public Function(Command initCommand, Collection<Token> parameterTokens) throws SLogoException  {
+    super("Function");
+    tokenList = parameterTokens;
+  }
+
+  private void parseParameterTokens(Command initCommand, Collection<Token> parameterTokens) throws SLogoException {
+
+  }
 
   /**
    * Adds the passed {@code Token} to the collection of parameters. This is used to create
@@ -21,8 +52,20 @@ public interface Function extends SLogoRunnable, slogo.WorkspaceEntry {
    * these {@code WorkspaceEntry} objects are set to the values that are passed to them.
    * @param params a {@code Collection} of parameter names.
    */
-  public void addParams(Collection<Token> params);
+  public void addParams(Collection<Token> params) {
 
+  }
+
+
+  @Override
+  public boolean isReady() {
+    return false;
+  }
+
+  @Override
+  public boolean giveNextExpectedToken(Token token) {
+    return false;
+  }
 
   /**
    * Calls {@code run()} recursively to take care of nested {@code SLogoRunnables}.
@@ -40,5 +83,7 @@ public interface Function extends SLogoRunnable, slogo.WorkspaceEntry {
    * @return the return value of the last runnable object, wrapped as a {@code Constant} token.
    */
   @Override
-  public Token run();
+  public Token run() {
+    return null;
+  }
 }
