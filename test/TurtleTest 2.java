cfad3fc79876cf7a0177f04643ee67a130ee8c43
Yi Chen <yc311@duke.edu>
2021-03-12 12:06:14 -0500

    Removed duplicate turtle command files

---
+++ test/TurtleTest 2.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayDeque;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import slogo.compiler.command.turtle.ForwardCommand;
+import slogo.compiler.token.SLogoFunction;
+import slogo.compiler.token.SLogoVariable;
+
+class TurtleTest {
+  @BeforeEach
+  void setup() {
+  }
+
+  @Test
+  void testForwardCommand() {
+    
+  }
+}
