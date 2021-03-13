0891c7d291500ff82ed503f36e1ab5edb4fde830
Kenneth Moore III <km460@duke.edu>
2021-03-12 22:32:29 -0500

    added 3 more tests and debug setHeading and added threshold for handling doubles

--- src/slogo/TurtleRunner.java
+++ src/slogo/TurtleRunner.java
@@ -3,10 +3,8 @@ package slogo;
 public class TurtleRunner {
   public static void main (String[] args) {
     Turtle t = new Turtle();
-    t.forward(10);
-    t.setHeading(180);
-    t.forward(10);
-    System.out.println(t.xCor()+" "+t.yCor());
+    t.towards(10,10);
+    System.out.println(t.xCor()+" "+t.yCor()+" "+t.heading());
   }
 
 }
--- test/TurtleTest.java
+++ test/TurtleTest.java
@@ -45,8 +45,36 @@ class TurtleTest {
     turtle.forward(10);
     assertEquals(0, turtle.xCor());
     assertEquals(0, turtle.yCor());
+    turtle.setHeading(45);
+    turtle.forward(14.14214);
+    assertEquals(10, turtle.xCor());
+    assertEquals(10, turtle.yCor());
   }
 
+  @Test
+  void testBack() {
+    turtle.back(100);
+    assertEquals(-100, turtle.yCor());
+    assertEquals(0, turtle.heading());
+  }
+
+  @Test
+  void testLeft() {
+    turtle.left(270);
+    assertEquals(90, turtle.heading());
+  }
+
+  @Test
+  void testRight() {
+    turtle.left(540);
+    assertEquals(180, turtle.heading());
+  }
+
+  @Test
+  void testTowards() {
+    turtle.towards(10,10);
+    assertEquals(45, turtle.heading());
+  }
 
   @Test
   void testForwardCommand() {
