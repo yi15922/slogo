dbea5b0fd95c04ee16f54d562ca018b6ee2992e8
Kenneth Moore III <km460@duke.edu>
2021-03-19 19:55:47 -0400

    added method headers to CompositeTurtle

--- src/slogo/model/CompositeTurtle.java
+++ src/slogo/model/CompositeTurtle.java
@@ -1,2 +1,93 @@
-package slogo.model;public class CompositeTurtle {
+package slogo.model;
+
+public class CompositeTurtle implements TurtleInterface
+  @Override
+  public double forward(double pixels) {
+    return 0;
+  }
+
+  @Override
+  public double back(double pixels) {
+    return 0;
+  }
+
+  @Override
+  public double left(double degrees) {
+    return 0;
+  }
+
+  @Override
+  public double right(double degrees) {
+    return 0;
+  }
+
+  @Override
+  public double setHeading(double degrees) {
+    return 0;
+  }
+
+  @Override
+  public double towards(double x, double y) {
+    return 0;
+  }
+
+  @Override
+  public double setXY(double x, double y) {
+    return 0;
+  }
+
+  @Override
+  public double penDown() {
+    return 0;
+  }
+
+  @Override
+  public double penUp() {
+    return 0;
+  }
+
+  @Override
+  public double showTurtle() {
+    return 0;
+  }
+
+  @Override
+  public double hideTurtle() {
+    return 0;
+  }
+
+  @Override
+  public double home() {
+    return 0;
+  }
+
+  @Override
+  public double clearScreen() {
+    return 0;
+  }
+
+  @Override
+  public double xCor() {
+    return 0;
+  }
+
+  @Override
+  public double yCor() {
+    return 0;
+  }
+
+  @Override
+  public double heading() {
+    return 0;
+  }
+
+  @Override
+  public int penDownP() {
+    return 0;
+  }
+
+  @Override
+  public int showingP() {
+    return 0;
+  }
 }
