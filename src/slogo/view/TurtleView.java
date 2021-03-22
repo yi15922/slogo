334fc409500b5ea892c12322adb6528c22bcf455
Yi Chen <yc311@duke.edu>
2021-03-21 23:30:48 -0400

    Waiting for new TurtleModelObserver interface to display multiple turtles

--- src/slogo/view/TurtleView.java
+++ src/slogo/view/TurtleView.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
+import slogo.observers.ModelObserver;
 
 /**
  * An image representation of a {@link slogo.model.SingleTurtle}. This object extends
@@ -13,21 +14,23 @@ import javafx.scene.shape.Circle;
  * be found in the resources folder, the turtle will be displayed as a circle by default.
  */
 // TODO: Make this contain both the image and the button somehow
-public class TurtleView extends ImageView {
+public class TurtleView extends ImageView implements ModelObserver {
 
   private Button myButton = new Button();
 
   private static final int WIDTH = 20;
+  private final int ID;
 
-  public TurtleView(String imageFileName){
+  public TurtleView(String imageFileName, int id){
 
     super.setImage(new Image(Objects
         .requireNonNull(this.getClass().getClassLoader().getResourceAsStream(imageFileName))));
+    ID = id;
     setFitWidth(WIDTH);
     setPreserveRatio(true);
     myButton.setMinSize(10, 10);
     myButton.setOpacity(0);
-    setOnMouseClicked(event -> System.out.println("User clicked"));
+    setOnMouseClicked(event -> System.out.println("User clicked " + ID));
 
   }
 
@@ -37,4 +40,38 @@ public class TurtleView extends ImageView {
     this.setLayoutY(y);
   }
 
+  @Override
+  public void receiveNewPosition(int id, double x, double y) {
+
+  }
+
+  @Override
+  public void receiveHeading(int id, double heading) {
+
+  }
+
+  @Override
+  public void checkPenStatus(int id, boolean b) {
+
+  }
+
+  @Override
+  public void receiveShow(int id, boolean show) {
+
+  }
+
+  @Override
+  public void receiveShape(int id, int shape) {
+
+  }
+
+  @Override
+  public void checkPenColor(int id, int index) {
+
+  }
+
+  @Override
+  public void checkPenSize(int id, double pixels) {
+
+  }
 }
